86554a30ce156bbd5a0859c386403360af2627e1
Akshay <akshaymanjunath26@gmail.com>
2024-09-26 18:24:04 +0530

    add: docker

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,13 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
+    // const connectionInstance = await mongoose.connect(
+    //   `${process.env.MONGODB_URL}/${DB_NAME}`
+    // );
+
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_NAME}`
-    );
+      'mongodb://localhost:27017/mydb'
+    )
     console.log("MongoDB connected !! ", connectionInstance.connection.host);
   } catch (error) {
     console.log("MongoDB connection FAILED! : ", error);
